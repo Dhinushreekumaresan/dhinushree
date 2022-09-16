d2d62605a1ade71a92dfc147f637e830f8d4beeb
Dhinushreekumaresan <dhinukumar2000@gmail.com>
2022-09-16 16:57:16 +0530

    My First Commit

---
+++ GitDemo/src/edu/training/git/GitDemoClass.java
@@ -0,0 +1,12 @@
+package edu.training.git;
+
+public class GitDemoClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hi Hello Git demo");
+
+	}
+
+}
